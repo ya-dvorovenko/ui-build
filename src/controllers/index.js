7341add88c4c7e1db4f73f08aa0ea65ec9401520
ya-dvorovenko <yadvorovenko@gmail.com>
2018-01-26 15:06:25 +0200

    removed vendors

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,2 @@
-export default class ExampleController {
-
+export default {
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const { resolve } = require('path');
 
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -131,7 +130,6 @@ const config = {
     }),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new ExtractTextPlugin({ filename: './styles/style.css', disable: false, allChunks: true }),
-    new CopyWebpackPlugin([{ from: 'vendors', to: 'vendors' }]),
     new OpenBrowserPlugin({ url: `http://localhost:${browserPort}` }),
     new webpack.HotModuleReplacementPlugin(),
   ],
